d8f3e30380bd48bc922e3e494e405e961dcd7848
callenegan <callen.egan@trulioo.com>
2021-06-08 15:57:44 -0700

    fix(script): something

--- server-build-script.sh
+++ server-build-script.sh
@@ -1,2 +1,2 @@
 echo "This is a test script to see if we can manually update the package.json file without a second commit"
-echo "next release version: ${nextRelease.version}"
+echo "next release version: $version"
