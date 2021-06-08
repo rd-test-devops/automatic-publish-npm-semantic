a190eb539c338e2b0e1c332f908a3e602d00a77e
callenegan <callen.egan@trulioo.com>
2021-06-08 14:34:06 -0700

    changed name

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/github",
+        ["@semantic-release/npm", {
+            npmPublish: false
+        }],
+        ["@semantic-release/release-notes-generator"],
+        ["@semantic-release/git", {
+            "assets": ["package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }]
+    ],
+    branches: ['main', 'next'],
+    preset: 'angular'
+}
