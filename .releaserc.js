7cf5b688d530c5f9f8460d36dfb2830b9d23c0b1
callenegan <callen.egan@trulioo.com>
2021-06-08 15:10:02 -0700

    fix(release file): modifying release file to see differences in commit message generated

--- .releaserc.js
+++ .releaserc.js
@@ -5,7 +5,7 @@ module.exports = {
         ["@semantic-release/npm", {
             npmPublish: false
         }],
-        //["@semantic-release/release-notes-generator"],
+        ["@semantic-release/release-notes-generator"],
         ["@semantic-release/git", {
             "assets": ["package.json"],
            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
