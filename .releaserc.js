7e9665ccbf179371f11c0427e5b16d4813d45cc4
callenegan <callen.egan@trulioo.com>
2021-06-08 15:07:16 -0700

    fix(release stuff): modifying release config file

--- .releaserc.js
+++ .releaserc.js
@@ -5,10 +5,10 @@ module.exports = {
         ["@semantic-release/npm", {
             npmPublish: false
         }],
-        ["@semantic-release/release-notes-generator"],
+        //["@semantic-release/release-notes-generator"],
         ["@semantic-release/git", {
             "assets": ["package.json"],
-           // "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+           "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }]
     ],
     branches: ['main', 'next'],
