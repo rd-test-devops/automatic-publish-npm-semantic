e782b3b24d7faaf888efeed66f0c99f089426c36
callenegan <callen.egan@trulioo.com>
2021-06-09 10:00:41 -0700

    feat(exec): removed exec command

--- .releaserc.js
+++ .releaserc.js
@@ -2,17 +2,17 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/github",
-        // ["@semantic-release/npm", {
-        //     npmPublish: false
-        // }],
+        ["@semantic-release/npm", {
+            npmPublish: false
+        }],
         ["@semantic-release/release-notes-generator"],
         ["@semantic-release/git", {
             "assets": ["package.json"],
            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
-        ["@semantic-release/exec", {
-            "prepareCmd": "./server-build-script.sh ${nextRelease.version}",
-        }],
+        // ["@semantic-release/exec", {
+        //     "prepareCmd": "./server-build-script.sh ${nextRelease.version}",
+        // }],
     ],
     branches: ['main', 'next'],
     preset: 'angular'
