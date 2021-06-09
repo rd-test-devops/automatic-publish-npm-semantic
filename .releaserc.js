3d793d1f804b993f86712940fd05be083359c2a6
callenegan <callen.egan@trulioo.com>
2021-06-09 11:18:18 -0700

    feat(reorder): reorder

--- .releaserc.js
+++ .releaserc.js
@@ -2,17 +2,14 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/github",
-        // ["@semantic-release/npm", {
-        //     npmPublish: false
-        // }],
+        ["@semantic-release/exec", {
+            "prepareCmd": "./server-build-script.sh ${nextRelease.version}",
+        }],
         ["@semantic-release/release-notes-generator"],
         ["@semantic-release/git", {
             "assets": ["package.json"],
            "message": "chore(release): ${nextRelease.version} [BUILD COMMIT]\n\n${nextRelease.notes}"
         }],
-        // ["@semantic-release/exec", {
-        //     "prepareCmd": "./server-build-script.sh ${nextRelease.version}",
-        // }],
     ],
     branches: ['main', 'next'],
     preset: 'angular'
