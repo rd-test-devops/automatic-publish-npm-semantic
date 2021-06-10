9be88493bb5a4e58b0be2ffe59067fdd2e3929eb
callenegan <callen.egan@trulioo.com>
2021-06-10 11:07:57 -0700

    feat(blah): blah blah

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,15 @@
 module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
-        "@semantic-release/github",
+        ["@semantic-release/release-notes-generator"],
         ["@semantic-release/exec", {
             "prepareCmd": "./server-build-script.sh ${nextRelease.version}",
         }],
-        ["@semantic-release/release-notes-generator"],
         ["@semantic-release/git", {
             "assets": ["package.json"],
            "message": "chore(release): ${nextRelease.version} [BUILD COMMIT]\n\n${nextRelease.notes}"
         }],
+        "@semantic-release/github",
     ],
     branches: ['main', 'next'],
     preset: 'angular'
