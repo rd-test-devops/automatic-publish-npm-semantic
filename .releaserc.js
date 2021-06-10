8de37107806c1d5d8499ac9915bbc5ffd872a6d6
callenegan <callen.egan@trulioo.com>
2021-06-10 11:27:18 -0700

    feat(release file): new release structure

--- .releaserc.js
+++ .releaserc.js
@@ -1,14 +1,20 @@
 module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
-        ["@semantic-release/release-notes-generator"],
-        ["@semantic-release/exec", {
-            "publishCmd": "./server-build-script.sh ${nextRelease.version}",
-        }],
-        ["@semantic-release/git", {
-            "assets": ["package.json"],
-           "message": "chore(release): ${nextRelease.version} [BUILD COMMIT]\n\n${nextRelease.notes}"
-        }],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/exec", {
+            "verifyReleaseCmd": "./server-build-script.sh ${nextRelease.version}",
+        },
+        "@semantic-release/changelog", {
+            "changelogFile": "CHANGELOG.md"
+        },
+        "@semantic-release/git", {
+            "npmPublish": false
+        },
+        "@semantic-release/git", {
+            "assets": ["package.json", "CHANGELOG.md"],
+            "message": "chore(release): ${nextRelease.version} [BUILD COMMIT]\n\n${nextRelease.notes}"
+        },
         "@semantic-release/github",
     ],
     branches: ['main', 'next'],
