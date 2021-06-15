81b85f09672227748d548a21f2da053d09abb5dd
callenegan <callen.egan@trulioo.com>
2021-06-15 14:12:19 -0700

    feat(pipeline): pipeline
    BREAKING CHANGE: alsjdlkasjdlfkaj

--- .releaserc.js
+++ .releaserc.js
@@ -8,9 +8,9 @@ module.exports = {
         "@semantic-release/changelog", {
             "changelogFile": "CHANGELOG.md"
         },
-        "@semantic-release/npm", {
-            "npmPublish": false
-        },
+        // "@semantic-release/npm", {
+        //     "npmPublish": false
+        // },
         "@semantic-release/git", {
             "assets": ["package.json", "CHANGELOG.md"],
             "message": "chore(release): ${nextRelease.version} [BUILD SPEC]\n\n${nextRelease.notes}"
